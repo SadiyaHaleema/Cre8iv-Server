8e66536f9e20033b9c4c05fc71b5a374c0c3f493
Sadiya Haleema <sadiyyahaleema282001@gmail.com>
2024-05-04 23:27:00 +0500

    app.js changed

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(cors(corsOptions));
 //app.use(express.urlencoded({ limit: '50mb', extended: false, parameterLimit: 50000 }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://cre8iv-frontend.vercel.app');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS,PUT,DELETE');
   res.header(
