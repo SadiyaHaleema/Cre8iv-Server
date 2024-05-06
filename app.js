853252640a04cdacd9db4d7379b3e51e402309ae
SadiyaHaleema <90968320+SadiyaHaleema@users.noreply.github.com>
2024-05-06 10:17:47 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(cors(
   {
-	origin: ["https://cre8iv-server-api.vercel.app/"],
+	origin: ["https://cre8iv-frontend-frontendapp.vercel.app/"],
 	methods: ["POST", "GET"],
 	credentials:true
 }
